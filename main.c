f12d5d1a7ddd5ca8b46eff240506f6be47605b37
bitabyte <anirudan2008@gmail.com>
2025-12-11 20:01:16 +0530

    read file

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+#include <string.h>
 
 char unsigned memory[30000] = {0};
 unsigned int stackpointer = 0;
 
 int main(int argc, char *argv[])
 {
-    
+
+    char bfc[1024];
+    FILE *fp;
+    fp = fopen(argv[1],"r");
+    fgets(bfc,sizeof(bfc),fp);
+    printf(bfc);
 }
 
