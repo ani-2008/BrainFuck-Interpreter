86f5e1efe468eb95532af6d54b1dac78224346ff
bitabyte <anirudan2008@gmail.com>
2025-12-19 20:16:42 +0530

    working +-<>.$

--- bfi.c
+++ bfi.c
@@ -10,7 +10,37 @@ int main(int argc, char *argv[])
     char bfc[1024];
     FILE *fp;
     fp = fopen(argv[1],"r");
-    fgets(bfc,sizeof(bfc),fp);
-    printf(bfc);
+
+    while(fgets(bfc,sizeof(bfc),fp) != NULL){
+        for(int i = 0; i < strlen(bfc); i++){
+            switch (bfc[i]){
+                case '+':
+                    memory[stackpointer]++;
+                    break;
+                case '-':
+                    memory[stackpointer]--;
+                    break;
+                case '>':
+                    stackpointer++;
+                    break;
+                case '<':
+                    stackpointer--;
+                    break;
+                case '.':
+                    printf("%c",memory[stackpointer]);
+                    break;
+                case '$':
+                    char c; 
+                    c = getchar();
+                    if (c == EOF){
+                        c = 0;
+                    }
+                    memory[stackpointer] = c;
+                    break;
+                    
+            }
+
+        }
+    }
 }
 
